aa9832c96892c037cb44b0e5c83eff430971df70
rearleg <frameonmoment@gmail.com>
2024-12-16 11:46:27 +0900

    feat : Print fibo with Binet

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
-def fibo(num) :
-    if num < 2 : 
-        return num
-    else :
-        return fibo(num-1) + fibo(num-2)
-print(fibo(5))
+#def fibo(num) :
+#    if num < 2 : 
+#        return num
+#    else :
+#        return fibo(num-1) + fibo(num-2)
+#print(fibo(5))
+from math import sqrt
+
+def fibo(num):
+    phi = (1 + math.sqrt(5)) / 2
+  
+    return round(pow(phi, n) / math.sqrt(5))
+
+print(fibo(5)
