f4e5b09d236167fc39199b0b926da47a90d7e75c
rearleg <frameonmoment@gmail.com>
2024-12-16 11:50:57 +0900

    feat : however Complete.

--- fibo.py
+++ fibo.py
@@ -4,11 +4,11 @@
 #    else :
 #        return fibo(num-1) + fibo(num-2)
 #print(fibo(5))
-from math import sqrt
+import math
 
 def fibo(num):
     phi = (1 + math.sqrt(5)) / 2
   
     return round(pow(phi, n) / math.sqrt(5))
 
-print(fibo(5)
+print(fibo(5))
